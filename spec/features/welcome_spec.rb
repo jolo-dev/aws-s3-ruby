e55391d7e39c8b0e8d4b1ff5979a7a973a0cb970
Paco Luna <pakoo_15@hotmail.com>
2019-10-15 12:35:48 -0500

    Add testing framework with some examples
    Rspec is a good test framework for the controllers and models, and
    integrated with capybara for the views is the best option for the
    project.

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,8 @@
+# require 'rails_helper'
+
+# RSpec.describe 'Welcome view', type: :feature do
+#   it 'has a title in welcome' do
+#     visit 'welcome/index'
+#     expect(page).to have_content('Welcome')
+#   end
+# end
